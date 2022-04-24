51f5f0f0e4c754189b98ee3b1ddc19610c419b8f
Justin <justin.b.alexander1@gmail.com>
2022-04-24 08:59:37 -0500

    cpp_script, dep files, includes, good make defaults

--- scripts/cpp_script_build.sh
+++ scripts/cpp_script_build.sh
@@ -5,29 +5,35 @@
 # source ~/dot-files/scripts/cpp_script_build.sh
 # #endif
 # EXTRA_FILES can be defined in that block to add multiple files to the compilation
-TARGET=/tmp/$(basename $0).run
+cd $(realpath $(dirname $0)) || { echo "Failed to cd to directory"; exit 1; }
+TARGET="$(realpath $0.run)"
 
 mkdir -p $(dirname $TARGET) || { echo "Failed to create $TARGET"; exit 1; }
 
-make -f - <<EOF
+make -j$(nproc) -f - <<EOF
+.DEFAULT_GOAL := all
+MAKEFLAGS += --no-builtin-rules --warn-undefined-variables --no-print-directory
+
 FILES = $0 $EXTRA_FILES
+INCLUDE_DIRS = .
 
 WARNINGS=-Wall -Wextra -Wformat -Wcast-align -Wcast-qual -Warray-compare -Wpointer-arith
-CFLAGS = -ggdb3 -std=gnu11 -fsanitize=undefined
-CPPFLAGS = -ggdb3 -std=gnu++11 -fsanitize=undefined
+CFLAGS = -ggdb3 -std=gnu11 -fsanitize=undefined \$(addprefix -I, \$(INCLUDE_DIRS))
+CPPFLAGS = -ggdb3 -std=gnu++11 -fsanitize=undefined \$(addprefix -I, \$(INCLUDE_DIRS))
 LDFLAGS = -fsanitize=undefined
 LIBS = -lubsan
 TARGET = $TARGET
 OBJECTS := \$(FILES:.c=.o)
 OBJECTS := \$(OBJECTS:.cpp=.o)
+-include \$(OBJECTS:.o=.d)
 
 all: \$(TARGET)
 \$(TARGET): \$(OBJECTS)
 	g++ -o \$@ \$(OBJECTS) \$(LDFLAGS) -Wl,--start-group \$(LIBS) -Wl,--end-group
 %.o: %.c
-	gcc -c \$< -o \$@ \$(CFLAGS)
+	gcc -c \$< -o \$@ -MMD -MP \$(CFLAGS)
 %.o: %.cpp
-	g++ -c \$< -o \$@ \$(CPPFLAGS)
+	g++ -c \$< -o \$@ -MMD -MP \$(CPPFLAGS)
 
 EOF
 
